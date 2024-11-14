d5fc48fc014bc6b2a7dc4da125f7913738e2a4a6
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-11-14 15:08:46 -0500

    make:make-icns.sh

---
+++ GNUmakefile
@@ -0,0 +1,2 @@
+-:
+	. make-icns.sh
--- make-icns.sh
+++ make-icns.sh
@@ -1,12 +1,15 @@
-mkdir icon.iconset
-sips -z 16 16     1024x1024.png --out icon.iconset/icon_16x16.png
-sips -z 32 32     1024x1024.png --out icon.iconset/icon_16x16@2x.png
-sips -z 32 32     1024x1024.png --out icon.iconset/icon_32x32.png
-sips -z 64 64     1024x1024.png --out icon.iconset/icon_32x32@2x.png
-sips -z 128 128   1024x1024.png --out icon.iconset/icon_128x128.png
-sips -z 256 256   1024x1024.png --out icon.iconset/icon_128x128@2x.png
-sips -z 256 256   1024x1024.png --out icon.iconset/icon_256x256.png
-sips -z 512 512   1024x1024.png --out icon.iconset/icon_256x256@2x.png
-sips -z 512 512   1024x1024.png --out icon.iconset/icon_512x512.png
+./make-sizes.sh
+mkdir -p icon.iconset
+sips -z 16 16     app/gnostr.png --out icon.iconset/icon_16x16.png
+sips -z 32 32     app/gnostr.png --out icon.iconset/icon_16x16@2x.png
+sips -z 32 32     app/gnostr.png --out icon.iconset/icon_32x32.png
+sips -z 64 64     app/gnostr.png --out icon.iconset/icon_32x32@2x.png
+sips -z 128 128   app/gnostr.png --out icon.iconset/icon_128x128.png
+sips -z 256 256   app/gnostr.png --out icon.iconset/icon_128x128@2x.png
+sips -z 256 256   app/gnostr.png --out icon.iconset/icon_256x256.png
+sips -z 512 512   app/gnostr.png --out icon.iconset/icon_256x256@2x.png
+sips -z 512 512   app/gnostr.png --out icon.iconset/icon_512x512.png
+sips -z 1024 1024   app/gnostr.png --out icon.iconset/icon_1024x1024.png
+sips -z 2048 2048   app/gnostr.png --out icon.iconset/icon_1024x1024@2x.png
 iconutil -c icns --output icon.icns icon.iconset
-rm -R icon.iconset
+#rm -R icon.iconset
